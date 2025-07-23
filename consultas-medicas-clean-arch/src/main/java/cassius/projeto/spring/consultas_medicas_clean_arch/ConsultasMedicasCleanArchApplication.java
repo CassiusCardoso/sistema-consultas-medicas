8481dd4e0a72485a0816d7728f5605ded61bfeae
CassiusCardoso <cassiuscardosoo@gmail.com>
2025-07-23 08:40:38 -0300

    chore: run da aplicação adicionado

---
+++ consultas-medicas-clean-arch/src/main/java/cassius/projeto/spring/consultas_medicas_clean_arch/ConsultasMedicasCleanArchApplication.java
@@ -0,0 +1,13 @@
+package cassius.projeto.spring.consultas_medicas_clean_arch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultasMedicasCleanArchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultasMedicasCleanArchApplication.class, args);
+	}
+
+}
