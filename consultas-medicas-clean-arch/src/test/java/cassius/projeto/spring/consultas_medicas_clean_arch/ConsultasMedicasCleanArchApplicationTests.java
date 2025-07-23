660f0179b47d0e99269cea34a3de0030cf8eeecd
CassiusCardoso <cassiuscardosoo@gmail.com>
2025-07-23 08:43:04 -0300

    chore: add application and test directory structure

---
+++ consultas-medicas-clean-arch/src/test/java/cassius/projeto/spring/consultas_medicas_clean_arch/ConsultasMedicasCleanArchApplicationTests.java
@@ -0,0 +1,13 @@
+package cassius.projeto.spring.consultas_medicas_clean_arch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultasMedicasCleanArchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
